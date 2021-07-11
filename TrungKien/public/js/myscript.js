f137756f49ccde236e77a222d68ce714d8fee977
Trung Kien <trungkien7300@gmail.com>
2021-07-12 00:47:55 +0700

    Edit a little bit on frontend

--- TrungKien/public/js/myscript.js
+++ TrungKien/public/js/myscript.js
@@ -25,29 +25,40 @@ $(document).ready(function ()
     {
         $('#img').click();
     });
-
-    $('#img').change(function ()
+    
+      $('#img').change(function ()
     {
         $("#all_images").html('');
         if ($(this)[0].files.length <= 4)
         {
             for (var i = 0; i < $(this)[0].files.length; i++)
             {
-                $("#all_images").append('<img  classs="avatar thumbnail" src="' + window.URL.createObjectURL(this.files[i]) + '" width="150px"  style="display:inline-block; margin-right: 1rem; margin-bottom: 1rem"/>');
+                $("#all_images").append('<img  classs="avatar thumbnail" src="' + window.URL.createObjectURL(this.files[i]) + '" width="130px" height="130px" style="display:inline-block; margin-right: 1rem; margin-bottom: 1rem;object-fit:cover"/>');
             }
         }
-        else{
+        else
+        {
             $("#all_images").html(`<p style="color:red;margin-bottom:1rem">Vui lòng nhập ít hơn 4 ảnh </p>`);
         }
     });
     $('#form_reset').click(function ()
     {
         $('#addProduct_form').trigger("reset");
-        $("#addCategory").val("");
         $("#addCategory").html(`<option disabled selected>Chọn Danh mục sản phẩm</option>`);
         $("#img").val("");
         $("#all_images").html("");
+        $('.alert').forEach(element =>
+        {
+            element.html("");
+        });
     });
-
+    $.each($(".update_image input"), function (i, value)
+    {
+        $(this).change(function (link)
+        {
+            console.log(this.files);
+            $(".update_image img")[i].src = window.URL.createObjectURL(this.files[0]);
+        })
+    })
 });
 
