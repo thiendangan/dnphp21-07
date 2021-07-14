0345c867123510070c3c60115dd32f3c2be87a38
Trung Kien <trungkien7300@gmail.com>
2021-07-14 09:16:47 +0700

    add invention package for resizing image before save

--- TrungKien/config/app.php
+++ TrungKien/config/app.php
@@ -229,6 +229,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Image' => Intervention\Image\Facades\Image::class,
+        'ImageSize' => App\ImageSize::class,
     ],
 
 ];
