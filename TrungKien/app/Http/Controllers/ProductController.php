1c1edc1815fca598dc9b9594405765dbe0498047
Trung Kien <trungkien7300@gmail.com>
2021-07-14 09:21:33 +0700

    fixed name and pagination with ORM

--- TrungKien/app/Http/Controllers/ProductController.php
+++ TrungKien/app/Http/Controllers/ProductController.php
@@ -1,4 +1,5 @@
-<?php
+ 
+ <?php
 
 namespace App\Http\Controllers;
 
@@ -6,10 +7,11 @@ use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\Category;
 use App\Models\ProductType;
-use CKSource\CKFinder\Image as CKFinderImage;
+use App\Http\Requests\StoreAndUpdateProductRequest;
 use Illuminate\Support\Str;
 use Image;
-use Illuminate\Pagination\Paginator;
+use ImageSize;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class ProductController extends Controller
 {
@@ -19,20 +21,13 @@ class ProductController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index($page = 1)
-    {
-        $products = new Product();
-        $temp =  $products->getproductData();
-        $temp1 = $products->getproductinfor();
-        for ($i = 0; $i < count($temp); $i++) {
-            $temp[$i]->product_category_name =  $temp1[$i];
-            $temp[$i]->product_image = explode(",", $temp[$i]->product_image)[0];
-        }
-        $temp = new \Illuminate\Pagination\Paginator($temp,5,$page);
-        $temp->withPath('/admin/product');
-        return view('product', ['products' => $temp, 'i' => 1, 'title' => $this->title]);
+    public function index()
+    {   
+        $products= Product::orderBy('created_at', 'DESC')->paginate(10);
+        $currentPage = $products->toArray()['current_page'];
+        $perPage = $products->toArray()['per_page'];
+        return view('product', ['products' => $products, 'index' => $perPage * ($currentPage - 1) + 1, 'title' => $this->title]);
     }
-
     /**
      * Show the form for creating a new resource.
      *
@@ -50,52 +45,19 @@ class ProductController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(StoreAndUpdateProductRequest $request)
     {
-        $request->validate([
-            'ProductName' => 'required|unique:product,product_name|max:200',
-            'Productprice' => 'required|numeric|max:1000000000|min:1',
-            'ProductType' => 'required',
-            'ProductCategory' => 'required',
-            'Description' => 'max:200',
-            'ProductImage' => "bail|required",
-            'ProductImage.*' => 'bail|image|mimes:jpg,png,jpeg|max:2048'
-        ], [
-            'ProductName.required' => "Vui lòng nhập tên sản phẩm",
-            'ProductName.unique' => "Tên đã tồn tại vui lòng nhập tên khác",
-            'ProductName.max' => 'Tên dài hơn 200 kí tự vui lòng nhập lại',
-            'Productprice.required' => "Vui lòng nhập giá của sản phẩm",
-            'Productprice.min'      => "vui lòng nhập giá sản phẩm theo thực tế",
-            'Productprice.max' => "Vui lòng nhập giá sản phẩm có số kí tự nhỏ hơn 10",
-            'Productprice.numeric' => "Vui lòng nhập giá sản phẩm bằng số",
-            'ProductType.required' => "Vui lòng chọn loại sản phẩm",
-            'ProductCategory.required' => "Vui lòng chọn danh mục sản phẩm",
-            'Description.max' => 'Vui lòng nhập description nhỏ hơn 200 kí tự',
-            'ProductImage.required' => 'Vui lòng upload ảnh sản phẩm',
-            'ProductImage.*.image'   => "vui lòng upload file ảnh",
-            'ProductImage.*.mimes'   => "vui lòng upload file .jpg, .jpeg ,.png",
-        ]);
-        $product = new Product();
-        $imagelinks = "";
-        // resize image demension and save
-        if ($request->hasfile('ProductImage')) {
-            foreach ($request->file('ProductImage') as $file) {
-                $newname = time().Str::random(4);
-                $filename =   $newname.'.'.$file->extension();
-                $image_resize = Image::make($file->getRealPath());
-                $image_resize->resize(960,600);
-                $image_resize->save(public_path() . '/ProductImage/'.$filename,80);
-                $imagelinks .=  $filename.",";
-            }
-            $product->product_image = $imagelinks;
-            $product->product_category_id  = $request->ProductCategory;
-            $product->product_id =  Str::random(5);
-            $product->product_name = $request->ProductName;
-            $product->product_price = $request->Productprice;
-            $product->product_description =  $request->Description;
-            $product->save();
-            return back()->with('success', 'Thêm mới sản phẩm thành công !');
-        }
+        $request->validated();
+        $product = new Product;
+        // resize image demension and save image name
+        $product->product_image = $this->resize($request, 'ProductImage');
+        $product->product_category_id  = $request->ProductCategory;
+        $product->product_id =  Str::random(5);
+        $product->product_name = ucfirst($request->ProductName);
+        $product->product_price = $request->Productprice;
+        $product->product_description = ucfirst($request->Description);
+        $product->save();
+        return back()->with('success', 'Thêm mới sản phẩm thành công !');
     }
 
     /**
@@ -106,13 +68,10 @@ class ProductController extends Controller
      */
     public function show($id)
     {
-        $product_infor =  Product::where('product_id',$id)->first();
-        $product_infor->product_image = explode(",",$product_infor->product_image);
-        $product_infor->ProductTypeName = Product::where('product_id',$id)->first()->get_category()->first()->get_producttype()->first()->product_type_name;
-        $product_infor->CategoryName = Product::where('product_id',$id)->first()->get_category()->first()->product_category_name;
-        return view('detailproduct',['title'=>$this->title,'product_infor' => $product_infor]);
+        // dung orm
+        $productInfor =  Product::where('product_id', $id)->first();
+        return view('detailproduct', ['title' => $this->title, 'productInfor' => $productInfor]);
     }
-
     /**
      * Show the form for editing the specified resource.
      *
@@ -122,11 +81,8 @@ class ProductController extends Controller
     public function edit($id)
     {
         $ProductTypes = ProductType::all('product_type_name');
-        $product_infor = Product::where('product_id',$id)->first();
-        $product_infor->product_image = explode(",",$product_infor->product_image);
-        $product_infor->ProductTypeName = Product::where('product_id',$id)->first()->get_category()->first()->get_producttype()->first()->product_type_name;
-        $product_infor->CategoryName = Product::where('product_id',$id)->first()->get_category()->first()->product_category_name;
-        return view('editproduct',['title' => $this->title, 'ProductTypes' => $ProductTypes,'product_infor' => $product_infor]);
+        $productInfor = Product::where('product_id', $id)->first();
+        return view('editproduct', ['title' => $this->title, 'ProductTypes' => $ProductTypes, 'productInfor' => $productInfor]);
     }
     /**
      * Update the specified resource in storage.
@@ -135,50 +91,16 @@ class ProductController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(StoreAndUpdateProductRequest $request, $id)
     {
-        $request->validate([
-            'ProductName' => 'required|max:200',
-            'Productprice' => 'required|numeric|max:1000000000|min:1',
-            'ProductType' => 'required',
-            'ProductCategory' => 'required',
-            'Description' => 'max:200',
-            'ProductImage' => "bail|required",
-            'ProductImage.*' => 'bail|image|mimes:jpg,png,jpeg|max:2048'
-        ], [
-            'ProductName.required' => "Vui lòng nhập tên sản phẩm",
-            'ProductName.unique' => "Tên đã tồn tại vui lòng nhập tên khác",
-            'ProductName.max' => 'Tên dài hơn 200 kí tự vui lòng nhập lại',
-            'Productprice.required' => "Vui lòng nhập giá của sản phẩm",
-            'Productprice.min'      => "vui lòng nhập giá sản phẩm theo thực tế",
-            'Productprice.max' => "Vui lòng nhập giá sản phẩm có số kí tự nhỏ hơn 10",
-            'Productprice.numeric' => "Vui lòng nhập giá sản phẩm bằng số",
-            'ProductType.required' => "Vui lòng chọn loại sản phẩm",
-            'ProductCategory.required' => "Vui lòng chọn danh mục sản phẩm",
-            'Description.max' => 'Vui lòng nhập description nhỏ hơn 200 kí tự',
-            'ProductImage.required' => 'Vui lòng upload ảnh sản phẩm 1',
-            'ProductImage.*.image'   => "vui lòng upload file ảnh",
-            'ProductImage.*.mimes'   => "vui lòng upload file .jpg, .jpeg ,.png",
-        ]);
-
+        $request->validated();
         $product = Product::where('product_id', $id)->first();
-
-        echo $product;
-        
-        $imagelinks = "";
-        if ($request->hasfile('ProductImage')){
-            foreach ($request->file('ProductImage') as $file) {
-                $name = $file->getClientOriginalName();
-                $file->move(public_path().'/ProductImage/', $name);
-                $imagelinks .= $name . ",";
-            }
-            $product->product_image = $imagelinks;
-            $product->product_category_id  = $request->ProductCategory;
-            $product->product_name = $request->ProductName;
-            $product->product_price = $request->Productprice;
-            $product->product_description =  $request->Description;
-            $product->save();
-        }
+        $product->product_image = $this->resize($request, 'ProductImage');
+        $product->product_category_id  = $request->ProductCategory;
+        $product->product_name = ucfirst( $request->ProductName);
+        $product->product_price = $request->Productprice;
+        $product->product_description = ucfirst($request->Description);
+        $product->save();
         return back()->with('success', 'Edit sản phẩm thành công !');
     }
     /**
@@ -190,13 +112,38 @@ class ProductController extends Controller
     public function destroy($id)
     {
         $product = Product::findOrFail($id);
-        $product_image = explode(",",$product->product_image);
-        foreach($product_image as $item){
-            if(!empty($item)){
-                unlink(public_path() . '/ProductImage/'.$item);
+        foreach ($product->product_image as $item) {
+            if (!empty($item)) {
+                unlink(public_path() . '/ProductImage/' . $item);
             }
         }
         $product->delete();
         return back()->with('success', 'Đã xóa  sản phẩm thành công !');
+    }
+
+    // function to settle when ajax call to get ProductType
+
+    public function productType(Request $request)
+    {
+        $categories =  ProductType::where('product_type_name', $request->category)->first()->categories;
+        return json_encode($categories, JSON_UNESCAPED_UNICODE);
+    }
+
+    // function to resize image when user uploaded
+
+    public function resize($request, $name)
+    {
+        $imagelinks = "";
+        if ($request->hasfile($name)) {
+            foreach ($request->file('ProductImage') as $file) {
+                $newName = time() . Str::random(4);
+                $fileName =   $newName . '.' . $file->extension();
+                $imageResize = Image::make($file->getRealPath());
+                $imageResize->resize(ImageSize::width, ImageSize::height);  // chuyen thag const
+                $imageResize->save(public_path() . '/ProductImage/' . $fileName, 80);
+                $imagelinks .=  $fileName . ",";
+            }
+        }
+        return $imagelinks;
     }
 }
--- TrungKien/app/Models/Product.php
+++ TrungKien/app/Models/Product.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\DB;
+
 class Product extends Model
 {
     use HasFactory;
@@ -12,23 +12,38 @@ class Product extends Model
     protected $primaryKey = 'product_id';
     public $incrementing = false;
     protected $keyType = 'string';
+    protected $appends = ['product_category_name', 'product_type_name'];
+
     // choose specific colum 
     //  get the  category from specific product
-    public function get_category(){
-        return $this->belongsTo('App\Models\Category','product_category_id','product_category_id');
-    }
-    // get all product in Product table
-    public function getproductData(){
-        return Product::orderBy('updated_at', 'DESC')->get();
-    }
-    public function getproductinfor(){
-        $products = Product::orderBy('updated_at', 'DESC')->get();
-        $array = array();
-        foreach($products as $product){
-            $array[] = $product->get_category->product_category_name;
-        }
-        return  $array;
+
+    public  function category()
+    {
+        return $this->belongsTo('App\Models\Category', 'product_category_id', 'product_category_id');
     }
     
+    public function getProductCategoryNameAttribute()
+    {
+        return $this->category->product_category_name;
+    }
 
+    public function getProductImageAttribute($value)
+    {
+        return  explode(",", $this->attributes['product_image']);
+    }
+    public function getProductTypeNameAttribute($value)
+    {
+        return $this->category->ProductType->product_type_name;
+    }
+
+
+    
+    // public static function getProductInfor(){
+    //     $products = Product::orderBy('updated_at', 'DESC')->get();
+    //     foreach($products as $product){
+    //         $product->product_category_name = $product->category->product_category_name;
+    //         $product->product_image = explode(",", $product->product_image)[0];
+    //     }
+    //     return  $products;
+    // }
 }
