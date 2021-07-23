695260fc76633ccffc012458d8209579ebefceac
Trung Kien <trungkien7300@gmail.com>
2021-07-23 14:14:09 +0700

    show detail of global_infor variable

--- TrungKien/app/Http/Controllers/HomeController.php
+++ TrungKien/app/Http/Controllers/HomeController.php
@@ -6,8 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\Category;
 use App\Models\Product;
 use App\Models\ProductType;
-use Illuminate\Support\Str;
-use PhpParser\Node\Expr\Cast\Object_;
+
 
 class HomeController extends Controller
 {
@@ -15,9 +14,9 @@ class HomeController extends Controller
     public function index(){
         // use count DB
         $global_infor = [];
-        $global_infor[]= Product::count();
-        $global_infor[] = Category::count();
-        $global_infor[] = ProductType::count();
+        $global_infor['product_amount']= Product::count();
+        $global_infor['category_amount'] = Category::count();
+        $global_infor['product_type_amount'] = ProductType::count();
         return view('index')->with('title',$this->title)->with('global_infor',$global_infor);
     }
 }
